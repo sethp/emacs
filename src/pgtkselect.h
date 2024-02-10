3825368d01e97fcc8455cd22cd8510bac1b45819
Seth Pellegrino <seth@codecopse.net>
2024-02-09 19:58:08 -0800

    Update copyright year to 2024
    Run "TZ=UTC0 admin/update-copyright".
    
    Do not merge.

--- src/pgtkselect.h
+++ src/pgtkselect.h
@@ -1,6 +1,6 @@
 /* Definitions and headers for selection of pure Gtk+3.
-   Copyright (C) 1989, 1993, 2005, 2008-2020 Free Software Foundation,
-   Inc.
+   Copyright (C) 1989, 1993, 2005, 2008-2020, 2024 Free Software
+   Foundation, Inc.
 
 This file is part of GNU Emacs.
 
